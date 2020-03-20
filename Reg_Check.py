fe283721578f3af9d438795735632062aaf00d77
andreykbjj <48283299+andreykbjj@users.noreply.github.com>
2020-03-20 14:08:05 +0200

    Update Reg_Check.py

--- Reg_Check.py
+++ Reg_Check.py
@@ -29,9 +29,9 @@ def get_value(gett: str):
 
 
 def get_stig_dictionary():
-    
+
     """Re-writes STIG title: value_name for human readable format"""
-    
+
     computer_settings = dict()
     for ks, vs in cats.items():
 
@@ -52,9 +52,9 @@ def get_stig_dictionary():
 
 
 def get_item_property():
-    
+
     """Function to create a Get-ItemProperty command list for all the registry values"""
-    
+
     new_listing = list()
     for kp, vp in cats.items():
 
@@ -79,9 +79,9 @@ def get_item_property():
 
 
 def read_pulled_txt(file_name: str):
-    
+
     """Function that reads the output from .txt exported file from .ps1 script with transcript"""
-    
+
     only_configs = list()
     pulled_configs_dict = dict()
     with open(file_name, 'r') as pulled:
@@ -100,21 +100,67 @@ def read_pulled_txt(file_name: str):
 
 
 def dry_check():
-    
+
     """Dumb Check to see what configs are not compliant with best practice"""
-    
-    for key, value in sorted(read_pulled_txt(file_name='andreyk-laptop.txt').items()):
+
+    not_compliant = dict()
+
+    for key, value in sorted(read_pulled_txt(file_name='laptop.txt').items()):
         for k, v in get_stig_dictionary().items():
+            # Default values check
             if key == v:
                 if str(value) == get_value(gett=k):
-                    print(k, colored(value, 'blue'), colored(get_value(gett=k), 'green'))
+                    # print(k, colored(value, 'blue'), colored(get_value(gett=k), 'green'))
+                    pass
                 elif str(value) != get_value(gett=k):
-                    print(k, colored(value, 'red'), colored(get_value(gett=k), 'green'))
+                    # print(k, colored(value, 'red'), colored(get_value(gett=k), 'green'))
+                    temp_dict = {k: [value, get_value(k)]}
+                    not_compliant.update(temp_dict)
+
+    # print(not_compliant)
+    return not_compliant
 
 
 def wet_check():
+
     """A function to deal with findings from dry_check function"""
+
+    shle = dict()
+
+    for wet_key, wet_value in dry_check().items():
+
+        best_practice = wet_value[1]
+        setting_in_place = wet_value[0]
+
+        # Stripping the "()" in an interesting way
+        table = str.maketrans(dict.fromkeys("()"))
+        parentheses_less = best_practice.translate(table)
+
+        temp_list = parentheses_less.split(' ')
+        if setting_in_place in temp_list:
+            # print(wet_key, colored(setting_in_place, 'blue'), colored(temp_list[temp_list.index(setting_in_place)],
+            # 'green'))
+            pass
+        # Shows only not matched
+        elif setting_in_place not in temp_list:
+            setting_in_place = setting_in_place.strip('{}')
+
+            # Runs another check after one more stripping
+            if setting_in_place not in temp_list:
+                print(wet_key, colored(setting_in_place, 'red'), colored(temp_list, 'green'))
+
+    print(shle)
+    return shle
+
+
+def super_wet_check():
+
+    """Another function to clear the compliant settings and leave behind only non-compliant.
+        this one has human (mine) logic in it"""
+
+    wet_check()
+
     return
 
 
-dry_check()
+super_wet_check()
