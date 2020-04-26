c96da76b6fc0e3869974699c8bc7558579bef55a
andreykbjj <48283299+andreykbjj@users.noreply.github.com>
2020-04-27 00:55:21 +0300

    Update csv_STIG_parser.py

--- csv_STIG_parser.py
+++ csv_STIG_parser.py
@@ -1,3 +1,4 @@
+import collections
 import pandas as pd
 
 # Re-usable variables
@@ -42,8 +43,12 @@ filteredData = list(filter(lambda x: any(True for c in key_names if c in x), df[
 
 # Filtering key names values ONLY to a list
 VALUES = list()
+COUNT = 0
+VAL_DICT = collections.defaultdict(list)
+
 for each in filteredData:
     each = each.split('\n')
+    COUNT = COUNT + 1
     for e in each:
         e = e.strip('\r')
         if len(e.split(':')) >= 2:
@@ -52,6 +57,7 @@ for each in filteredData:
                 filteredNone = [filteredNone[0], filteredNone[1].lstrip(' ')]
                 if filteredNone[0] in key_names:
                     VALUES.append(filteredNone)
+                    VAL_DICT[COUNT].append(filteredNone)
 
 # 4 lists for each type of Data that extracted from 'values' list
 REG_HIVE = list()
@@ -59,15 +65,25 @@ REG_PATH = list()
 REG_NAME = list()
 REG_VALUE = list()
 
-for v in VALUES:
-    if v[0] == key_names[0]:
-        REG_HIVE.append(v[1])
-    elif v[0] == key_names[1]:
-        REG_PATH.append(v[1])
-    elif v[0] == key_names[2]:
-        REG_NAME.append(v[1])
-    elif v[0] == key_names[3]:
-        REG_VALUE.append(v[1])
+for k, v in VAL_DICT.items():
+    # Check for missing data UN-DONE
+    if len(v) > 4:
+        # print(v)
+        pass
+    # Checking list length for missing [Value, ''] in a 'v' list
+    if v[-1][0] != key_names[3]:
+        v.append(['Value', None])
+    if v[0][0] == key_names[0]:
+        REG_HIVE.append(v[0][1])
+    if v[1][0] == key_names[1]:
+        REG_PATH.append(v[1][1])
+    if v[2][0] == key_names[2]:
+        REG_NAME.append(v[2][1])
+    if v[-1][0] == key_names[3]:
+        REG_VALUE.append(v[-1][1])
+
+# FOR TESTING
+# print(len(REG_HIVE), len(REG_PATH), len(REG_NAME), len(REG_VALUE))
 
 # Creating 4 columns and adding corresponding data to each column
 df = df.assign(**{STR_REG_HIVE: REG_HIVE[0:193],
@@ -75,8 +91,7 @@ df = df.assign(**{STR_REG_HIVE: REG_HIVE[0:193],
                   STR_VALUE: REG_VALUE[0:193],
                   STR_VALUE_NAME: REG_NAME[0:193]})
 
-print(len(REG_HIVE), len(REG_PATH), len(REG_VALUE), len(REG_NAME))
-
+print(df)
 
 
 # Get-ItemProperty -Path "HKLM:\Software\Policies\Microsoft\Windows\PowerShell\ScriptBlockLogging\" | fl EnableScriptBlockLogging
